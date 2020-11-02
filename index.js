a9f028796235f942d60dd99d8d97d68af8e22c2e
e <e>
2020-11-02 15:47:15 +1300

    Add data route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
+var path = require('path');
 const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.get('/data', (req, res) => {
+    res.sendFile(path.join(__dirname + '/data.json'));
 })
 
 app.listen(port, () => {
